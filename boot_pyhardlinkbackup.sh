d7b3531ea2f0d544713b858e5620139cb3df2706
Jens Diemer <git@jensdiemer.de>
2016-01-10 14:29:10 +0100

    add boot script for linux

---
+++ boot_pyhardlinkbackup.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+DESTINATION=~/PyHardLinkBackup
+
+(
+    set -e
+    set -x
+
+    python3 --version
+
+    rm -Rf ${DESTINATION}
+
+    python3 -m venv --without-pip ${DESTINATION}
+
+    ls -la ${DESTINATION}/bin
+)
+(
+    set -x
+    ${DESTINATION}/bin/python -m ensurepip
+)
+if [ "$?" == "0" ]; then
+    echo "pip installed, ok"
+else
+    echo "ensurepip doesn't exist, use get-pip.py"
+    (
+        set -e
+        set -x
+        cd ${DESTINATION}/bin
+        wget https://bootstrap.pypa.io/get-pip.py
+        ${DESTINATION}/bin/python get-pip.py
+    )
+fi
+(
+    set -e
+    set -x
+    ${DESTINATION}/bin/pip install --upgrade pip
+
+    ${DESTINATION}/bin/pip install PyHardLinkBackup
+
+    ${DESTINATION}/bin/phlb_setup_helper_files
+)
