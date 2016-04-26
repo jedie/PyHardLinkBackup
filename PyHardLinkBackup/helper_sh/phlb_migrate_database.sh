911da000a78fd0bf7ec5ae02c93e3619bd1ddbfa
JensDiemer <git@jensdiemer.de>
2016-04-26 16:41:27 +0200

    add migrate scripts

---
+++ PyHardLinkBackup/helper_sh/phlb_migrate_database.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+ENV_PATH=~/PyHardLinkBackup
+CURRENT_DIR=$(pwd)
+
+set -e
+
+ACTIVATE_SCRIPT=${ENV_PATH}/bin/activate
+echo "Activate venv with: ${ACTIVATE_SCRIPT}"
+source ${ACTIVATE_SCRIPT}
+
+set -x
+
+manage migrate
