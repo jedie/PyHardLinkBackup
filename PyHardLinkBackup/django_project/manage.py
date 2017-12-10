5a7cffc7266da9321f925d5d791a118380500cee
JensDiemer <git@jensdiemer.de>
2017-12-10 16:40:04 +0100

    set DJANGO_SETTINGS_MODULE

--- PyHardLinkBackup/django_project/manage.py
+++ PyHardLinkBackup/django_project/manage.py
@@ -1,9 +1,15 @@
+#!/usr/bin/env python
 
+import os
 import sys
+
 
 def cli():
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
 
+
 if __name__ == '__main__':
+    # Needed if direct called
+    os.environ["DJANGO_SETTINGS_MODULE"] = "PyHardLinkBackup.django_project.settings"
     cli()
