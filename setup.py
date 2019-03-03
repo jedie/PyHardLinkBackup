5ff659678a4f9f35ee8e926a8abe3261daa75f56
JensDiemer <git@jensdiemer.de>
2019-03-03 20:43:20 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -18,6 +18,8 @@ from __future__ import absolute_import, division, print_function
 
 
 import os
+import shutil
+import subprocess
 import sys
 
 from setuptools import setup, find_packages
@@ -28,25 +30,25 @@ from PyHardLinkBackup import __version__
 PACKAGE_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
-# convert README.creole on-the-fly to ReSt, see also:
-# https://github.com/jedie/python-creole/wiki/Use-In-Setup/
-check_readme="publish" in sys.argv or "check" in sys.argv or "register" in sys.argv or "sdist" in sys.argv or "--long-description" in sys.argv
-try:
-    from creole.setup_utils import get_long_description
-except ImportError as err:
-    if check_readme:
-        raise ImportError("%s - Please install python-creole >= v0.8 -  e.g.: pip install python-creole" % err)
-    long_description = None
-else:
-    if check_readme:
-        print("\nCheck creole2ReSt:")
-    long_description = get_long_description(PACKAGE_ROOT)
-    if check_readme:
-        print("OK")
-
+# _____________________________________________________________________________
+# convert creole to ReSt on-the-fly, see also:
+# https://github.com/jedie/python-creole/wiki/Use-In-Setup
+long_description = None
+for arg in ("test", "check", "register", "sdist", "--long-description"):
+    if arg in sys.argv:
+        try:
+            from creole.setup_utils import get_long_description
+        except ImportError as err:
+            raise ImportError("%s - Please install python-creole - e.g.: pip install python-creole" % err)
+        else:
+            long_description = get_long_description(PACKAGE_ROOT)
+        break
+# ----------------------------------------------------------------------------
 
 if "publish" in sys.argv:
     """
+    'publish' helper for setup.py
+
     Build and upload to PyPi, if...
         ... __version__ doesn't contains "dev"
         ... we are on git 'master' branch
@@ -59,31 +61,36 @@ if "publish" in sys.argv:
      * use testpypi................: ./setup.py publish --repository=test
 
     TODO: Look at: https://github.com/zestsoftware/zest.releaser
-    """
-    # Imports here, so it's easier to copy&paste this complete code block ;)
-    import subprocess
-    import shutil
 
+    Source: https://github.com/jedie/python-code-snippets/blob/master/CodeSnippets/setup_publish.py
+    copyleft 2015-2019 Jens Diemer - GNU GPL v2+
+    """
+    assert sys.version_info[0] > 2, "Python v3 is needed!"
+
+    import_error = False
     try:
         # Test if wheel is installed, otherwise the user will only see:
         #   error: invalid command 'bdist_wheel'
         import wheel
     except ImportError as err:
         print("\nError: %s" % err)
-        print("\nMaybe https://pypi.python.org/pypi/wheel is not installed or virtualenv not activated?!?")
+        print("\nMaybe https://pypi.org/project/wheel is not installed or virtualenv not activated?!?")
         print("e.g.:")
         print("    ~/your/env/$ source bin/activate")
         print("    ~/your/env/$ pip install wheel")
-        sys.exit(-1)
+        import_error = True
 
     try:
         import twine
     except ImportError as err:
         print("\nError: %s" % err)
-        print("\nMaybe https://pypi.python.org/pypi/twine is not installed or virtualenv not activated?!?")
+        print("\nMaybe https://pypi.org/project/twine is not installed or virtualenv not activated?!?")
         print("e.g.:")
         print("    ~/your/env/$ source bin/activate")
         print("    ~/your/env/$ pip install twine")
+        import_error = True
+
+    if import_error:
         sys.exit(-1)
 
     def verbose_check_output(*args):
@@ -102,9 +109,16 @@ if "publish" in sys.argv:
         print("\tCall: %r\n" % " ".join(args))
         subprocess.check_call(args, universal_newlines=True)
 
-    if "dev" in __version__:
-        print("\nERROR: Version contains 'dev': v%s\n" % __version__)
-        sys.exit(-1)
+    def confirm(txt):
+        print("\n%s" % txt)
+        if input("\nPublish anyhow? (Y/N)").lower() not in ("y", "j"):
+            print("Bye.")
+            sys.exit(-1)
+
+    for key in ("dev", "rc"):
+        if key in __version__:
+            confirm("WARNING: Version contains %r: v%s\n" % (key, __version__))
+            break
 
     print("\nCheck if we are on 'master' branch:")
     call_info, output = verbose_check_output("git", "branch", "--no-color")
@@ -112,11 +126,7 @@ if "publish" in sys.argv:
     if "* master" in output:
         print("OK")
     else:
-        print("\nNOTE: It seems you are not on 'master':")
-        print(output)
-        if input("\nPublish anyhow? (Y/N)").lower() not in ("y", "j"):
-            print("Bye.")
-            sys.exit(-1)
+        confirm("\nNOTE: It seems you are not on 'master':\n%s" % output)
 
     print("\ncheck if if git repro is clean:")
     call_info, output = verbose_check_output("git", "status", "--porcelain")
@@ -127,6 +137,14 @@ if "publish" in sys.argv:
         print("\n *** ERROR: git repro not clean:")
         print(output)
         sys.exit(-1)
+
+    print("\nRun './setup.py check':")
+    call_info, output = verbose_check_output("./setup.py", "check")
+    if "warning" in output:
+        print(output)
+        confirm("Warning found!")
+    else:
+        print("OK")
 
     print("\ncheck if pull is needed")
     verbose_check_call("git", "fetch", "--all")
@@ -141,28 +159,37 @@ if "publish" in sys.argv:
     verbose_check_call("git", "push")
 
     print("\nCleanup old builds:")
+
     def rmtree(path):
         path = os.path.abspath(path)
         if os.path.isdir(path):
             print("\tremove tree:", path)
             shutil.rmtree(path)
+
     rmtree("./dist")
     rmtree("./build")
 
     print("\nbuild but don't upload...")
-    log_filename="build.log"
+    log_filename = "build.log"
     with open(log_filename, "a") as log:
         call_info, output = verbose_check_output(
-            sys.executable or "python",
-            "setup.py", "sdist", "bdist_wheel", "bdist_egg"
+            sys.executable or "python", "setup.py", "sdist", "bdist_wheel", "bdist_egg"
         )
         print("\t%s" % call_info)
         log.write(call_info)
         log.write(output)
     print("Build output is in log file: %r" % log_filename)
 
-    print("\ngit tag version (will raise a error of tag already exists)")
-    verbose_check_call("git", "tag", "v%s" % __version__)
+    git_tag = "v%s" % __version__
+
+    print("\ncheck git tag")
+    call_info, output = verbose_check_output("git", "log", "HEAD..origin/master", "--oneline")
+    if git_tag in output:
+        print("\n *** ERROR: git tag %r already exists!" % git_tag)
+        print(output)
+        sys.exit(-1)
+    else:
+        print("OK")
 
     print("\nUpload with twine:")
     twine_args = sys.argv[1:]
@@ -170,12 +197,17 @@ if "publish" in sys.argv:
     twine_args.insert(1, "dist/*")
     print("\ttwine upload command args: %r" % " ".join(twine_args))
     from twine.commands.upload import main as twine_upload
+
     twine_upload(twine_args)
+
+    print("\ngit tag version")
+    verbose_check_call("git", "tag", git_tag)
 
     print("\ngit push tag to server")
     verbose_check_call("git", "push", "--tags")
 
     sys.exit(0)
+
 
 
 def get_authors():
@@ -185,6 +217,7 @@ def get_authors():
     except Exception as err:
         authors = "[Error: %s]" % err
     return authors
+
 
 setup(
     name='PyHardLinkBackup',
