e33a6afb3cb50c583fd28e100d909acd45ab5281
JensDiemer <git@jensdiemer.de>
2020-02-02 20:55:11 +0100

    remove unused import

--- pyhardlinkbackup/django_project/urls.py
+++ pyhardlinkbackup/django_project/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 
-from django.conf.urls import include, url
+from django.conf.urls import url
 from django.contrib import admin
 
 # https://github.com/jedie/PyHardLinkBackup
